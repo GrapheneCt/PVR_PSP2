8725a27899b58e2e8cb78fef00dc6bcbf4ccd427
bythos14 <45720552+bythos14@users.noreply.github.com>
2021-07-04 15:17:25 -0400

    Improvements to sgx_render_flip_test
    Now it crashes due to page fault.

--- include/gpu_es4/psp2_pvr_desc.h
+++ include/gpu_es4/psp2_pvr_desc.h
@@ -19,4 +19,10 @@
 #define TRANSFER_QUEUE
 #define SGX_CORE_REV 113 //Actual revision on Vita is 117, but there is no support for it in this driver version
 
+// Defined here due to driver bug in revision 117 not having a bug present in revision 113
+#define EUR_CR_PDS_PP_INDEPENDANT_STATE     0x0AE8
+#define EUR_CR_PDS_PP_INDEPENDANT_STATE_DISABLE_MASK 0x00000001U
+#define EUR_CR_PDS_PP_INDEPENDANT_STATE_DISABLE_SHIFT 0
+#define EUR_CR_PDS_PP_INDEPENDANT_STATE_DISABLE_SIGNED 0
+
 #endif
