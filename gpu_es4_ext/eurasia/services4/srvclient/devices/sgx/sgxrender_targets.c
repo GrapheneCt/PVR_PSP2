1b51cdf3eb2549af7e3c767c7a58f2879433b9c5
bythos14 <45720552+bythos14@users.noreply.github.com>
2021-12-11 19:28:05 -0400

    Fix SGXGetRenderTargetMemSize

--- gpu_es4_ext/eurasia/services4/srvclient/devices/sgx/sgxrender_targets.c
+++ gpu_es4_ext/eurasia/services4/srvclient/devices/sgx/sgxrender_targets.c
@@ -10,14 +10,15 @@
 #include <sgx_mkif_km.h>
 #include <sgx_mkif_client.h>
 #include <servicesint.h>
+#include <pvr_debug.h>
 
 #define SGX_MT_DEFAULT_THRESHOLD (640 * 480) // Threshold for 4x4 MT configuration
 
 #define ALIGN(x, a)	(((x) + ((a) - 1)) & ~((a) - 1))
 
-#define INCR_MEMSIZE(size, alignment) ui32MemSize = ALIGN(ui32MemSize, alignment); \
- ui32MemSize = (ALIGN(ui32MemSize, PVRSRV_4K_PAGE_SIZE) - ui32MemSize) < size ? ALIGN(ui32MemSize, PVRSRV_4K_PAGE_SIZE) : ui32MemSize; \
- ui32MemSize += size
+#define INCR_MEMSIZE(memSize, size, alignment) \
+ memSize = (ALIGN(memSize, PVRSRV_4K_PAGE_SIZE) - memSize) < (size + (memSize & (alignment - 1))) ? ALIGN(memSize, PVRSRV_4K_PAGE_SIZE) : ALIGN(memSize, alignment); \
+ memSize += size
 
 static IMG_UINT32 NextPowerOfTwo(IMG_UINT32 ui32Value) {
 	ui32Value--;
@@ -43,15 +44,32 @@ SGXGetRenderTargetMemSize(SGX_ADDRENDTARG *psAddRTInfo, IMG_UINT32 *pui32MemSize
 	IMG_UINT32 ui32TilesInX, ui32TilesInY;
 	IMG_UINT32 ui32MTilesInX, ui32MTilesInY;
 	IMG_UINT32 ui32TilesPerMTileX, ui32TilesPerMTileY;
-	IMG_UINT32 ui32MemSize = 0;
 	IMG_UINT32 ui32TailPtrsSize;
 	IMG_UINT32 ui32RgnSize;
 	IMG_UINT32 ui32MacroTileRgnLUTSize;
+	IMG_UINT32 ui32KernelDataMemSize[2] = { 0 }, ui323DParamMemSize[2] = { 0 }, ui32SyncDataMemSize = 0, ui32TADataMemSize = 0;
+	IMG_UINT32 ui32MemSize = 0;
 	IMG_INT i, j;
 
 	if (psAddRTInfo == IMG_NULL || pui32MemSize == IMG_NULL)
 	{
+		PVR_DPF((PVR_DBG_ERROR, "SGXGetRenderTargetMemSize: Invalid pointer"));
 		return PVRSRV_ERROR_INVALID_PARAMS;
+	}
+
+	if (psAddRTInfo->ui32Flags & SGX_ADDRTFLAGS_CUSTOM_MACROTILE_COUNTS)
+	{
+		if (psAddRTInfo->ui8MacrotileCountX > 4 || psAddRTInfo->ui8MacrotileCountX < 1)
+		{
+			PVR_DPF((PVR_DBG_ERROR, "SGXGetRenderTargetMemSize: Macrotile count on the X axis (%d macrotiles) is not within the valid range [1, 4]", psAddRTInfo->ui8MacrotileCountX));
+			return PVRSRV_ERROR_INVALID_PARAMS;
+		}
+
+		if (psAddRTInfo->ui8MacrotileCountY > 4 || psAddRTInfo->ui8MacrotileCountY < 1)
+		{
+			PVR_DPF((PVR_DBG_ERROR, "SGXGetRenderTargetMemSize: Macrotile count on the Y axis (%d macrotiles) is not within the valid range [1, 4]", psAddRTInfo->ui8MacrotileCountY));
+			return PVRSRV_ERROR_INVALID_PARAMS;
+		}
 	}
 
 	ui32TilesInX = (psAddRTInfo->ui32NumPixelsX + (EURASIA_ISPREGION_SIZEX - 1)) / EURASIA_ISPREGION_SIZEX;
@@ -139,31 +157,30 @@ SGXGetRenderTargetMemSize(SGX_ADDRENDTARG *psAddRTInfo, IMG_UINT32 *pui32MemSize
 
 	for (i = 0; i < psAddRTInfo->ui32MaxQueuedRenders; i++)
 	{
-		INCR_MEMSIZE(sizeof(SGXMKIF_HWRENDERDETAILS), 32);
-		INCR_MEMSIZE(sizeof(PVRSRV_RESOURCE), 4);
+		INCR_MEMSIZE(ui32KernelDataMemSize[0], sizeof(SGXMKIF_HWRENDERDETAILS), 32);
+		INCR_MEMSIZE(ui32SyncDataMemSize, sizeof(PVRSRV_RESOURCE), 4);
 
-		INCR_MEMSIZE(sizeof(SGXMKIF_HWDEVICE_SYNC_LIST), 32);
-		INCR_MEMSIZE(sizeof(PVRSRV_RESOURCE), 4);
+		INCR_MEMSIZE(ui32KernelDataMemSize[0], sizeof(SGXMKIF_HWDEVICE_SYNC_LIST) + sizeof(PVRSRV_DEVICE_SYNC_OBJECT), 32);
+		INCR_MEMSIZE(ui32SyncDataMemSize, sizeof(PVRSRV_RESOURCE), 4);
 	}
 
-	INCR_MEMSIZE(sizeof(IMG_UINT32), 32);
-	INCR_MEMSIZE(sizeof(SGXMKIF_HWRTDATASET) + sizeof(SGXMKIF_HWRTDATA) * (psAddRTInfo->ui32NumRTData - 1), 32);
+	INCR_MEMSIZE(ui32KernelDataMemSize[1], sizeof(IMG_UINT32), 32);
+	INCR_MEMSIZE(ui32KernelDataMemSize[1], sizeof(SGXMKIF_HWRTDATASET) + sizeof(SGXMKIF_HWRTDATA) * (psAddRTInfo->ui32NumRTData - 1), 32);
 
 	ui32TailPtrsSize = MAX(NextPowerOfTwo(ui32MTilesInX * ui32TilesPerMTileX), NextPowerOfTwo(ui32MTilesInY * ui32TilesPerMTileY));
 	ui32TailPtrsSize = ui32TailPtrsSize * ui32TailPtrsSize * EURASIA_TAILPOINTER_SIZE;
 
 	for (i = 0; i < SGX_FEATURE_MP_CORE_COUNT_TA; i++)
 	{
-		INCR_MEMSIZE(ui32TailPtrsSize, EURASIA_PARAM_MANAGE_TPC_GRAN);
-		INCR_MEMSIZE(EURASIA_PARAM_MANAGE_CONTROL_SIZE, EURASIA_PARAM_MANAGE_CONTROL_GRAN);
-		INCR_MEMSIZE(EURASIA_PARAM_MANAGE_OTPM_SIZE, EURASIA_PARAM_MANAGE_OTPM_GRAN);
+		INCR_MEMSIZE(ui32TADataMemSize, ui32TailPtrsSize, EURASIA_PARAM_MANAGE_TPC_GRAN);
+		INCR_MEMSIZE(ui32TADataMemSize, EURASIA_PARAM_MANAGE_CONTROL_SIZE, EURASIA_PARAM_MANAGE_CONTROL_GRAN);
+		INCR_MEMSIZE(ui32TADataMemSize, EURASIA_PARAM_MANAGE_OTPM_SIZE, EURASIA_PARAM_MANAGE_OTPM_GRAN);
 	}
 
-	INCR_MEMSIZE(SGX_SPECIALOBJECT_SIZE, EURASIA_PARAM_MANAGE_GRAN);
-	INCR_MEMSIZE(EURASIA_PARAM_MANAGE_PIM_SIZE, EURASIA_PARAM_MANAGE_PIM_GRAN);
+	INCR_MEMSIZE(ui323DParamMemSize[0], SGX_SPECIALOBJECT_SIZE, EURASIA_PARAM_MANAGE_GRAN);
+	INCR_MEMSIZE(ui323DParamMemSize[1], EURASIA_PARAM_MANAGE_PIM_SIZE, EURASIA_PARAM_MANAGE_PIM_GRAN);
 
 	ui32RgnSize = (ui32MTilesInX * ui32TilesPerMTileX) * (ui32MTilesInY * ui32TilesPerMTileY) * EURASIA_REGIONHEADER_SIZE;
-	ui32RgnSize = ALIGN(ui32RgnSize, 1 << 8);
 
 	ui32MacroTileRgnLUTSize = ((ui32MTilesInX * ui32MTilesInY) * sizeof(IMG_UINT32)) * SGX_FEATURE_MP_CORE_COUNT_TA;
 
@@ -171,13 +188,22 @@ SGXGetRenderTargetMemSize(SGX_ADDRENDTARG *psAddRTInfo, IMG_UINT32 *pui32MemSize
 	{
 		for (j = 0; j < SGX_FEATURE_MP_CORE_COUNT_TA; j++)
 		{
-			INCR_MEMSIZE(ui32RgnSize, EURASIA_PARAM_MANAGE_REGION_GRAN);
+			INCR_MEMSIZE(ui32TADataMemSize, ui32RgnSize, EURASIA_PARAM_MANAGE_REGION_GRAN);
 		}
-		INCR_MEMSIZE(EURASIA_PARAM_MANAGE_STATE_SIZE, EURASIA_PARAM_MANAGE_STATE_GRAN);
-		INCR_MEMSIZE(ui32MacroTileRgnLUTSize, 32);
+		INCR_MEMSIZE(ui32TADataMemSize, EURASIA_PARAM_MANAGE_STATE_SIZE, EURASIA_PARAM_MANAGE_STATE_GRAN);
+		INCR_MEMSIZE(ui32KernelDataMemSize[1], ui32MacroTileRgnLUTSize, 32);
 	}
 
-	*pui32MemSize = ALIGN(ui32MemSize, PVRSRV_4K_PAGE_SIZE);
+	for (int i = 0; i < 2; i++)
+	{
+		ui32MemSize += ALIGN(ui323DParamMemSize[i], PVRSRV_4K_PAGE_SIZE);
+		ui32MemSize += ALIGN(ui32KernelDataMemSize[i], PVRSRV_4K_PAGE_SIZE);
+	}
+
+	ui32MemSize += ALIGN(ui32SyncDataMemSize, PVRSRV_4K_PAGE_SIZE);
+	ui32MemSize += ALIGN(ui32TADataMemSize, PVRSRV_4K_PAGE_SIZE);
+
+	*pui32MemSize = ui32MemSize;
 
 	return PVRSRV_OK;
 }
