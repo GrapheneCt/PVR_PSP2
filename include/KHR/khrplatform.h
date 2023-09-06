ae4df4b723f3f1aa629b1f3b276b9cca6d663891
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2023-09-06 15:32:18 +0200

    Enabled basic KHR definitions for cpp compilations on SNC

--- include/KHR/khrplatform.h
+++ include/KHR/khrplatform.h
@@ -142,7 +142,7 @@
 /*-------------------------------------------------------------------------
  * basic type definitions
  *-----------------------------------------------------------------------*/
-#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__GNUC__) || defined(__SCO__) || defined(__USLC__)
+#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__GNUC__) || defined(__SCO__) || defined(__USLC__) || defined(__SNC__)
 
 /*
  * Using <stdint.h>
